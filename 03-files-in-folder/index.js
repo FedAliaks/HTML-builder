a8b0910b5d5c379d83a2ab3b3c669cd03b99870c
Aliaksandr Fedukovich <1632637@mail.ru>
2023-05-07 16:11:20 +0300

    feat: add solution for task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) =>{
+  if(err) console.log(err.message);
+  files.forEach(item => {
+    const pathToFile = path.join(pathToFolder, item.name);
+    fs.stat(pathToFile, (err, statistic) => {
+      if(item.isFile()) {
+        console.log(path.parse(pathToFile).name + " - " + path.parse(pathToFile).ext.slice(1) + " - " + `${(statistic.size / 1024).toFixed(2)}Kb`);
+      }
+    })
+  })
+
+})
