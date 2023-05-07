da2232d1455e7172d3611cb5b735c97a5e80398d
Aliaksandr Fedukovich <1632637@mail.ru>
2023-05-07 17:40:40 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const promise = require('fs/promises');
 
+try {
+  promise.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+
+  const pathToSourceFolder = path.join(__dirname, 'files');
+  const pathToGoalFolder = path.join(__dirname, 'files-copy');
+
+  fs.readdir(pathToGoalFolder, (err, files) => {
+    if (err) {
+      console.log('error')
+    }
+    files.forEach(item => {
+      fs.unlink(path.join(pathToGoalFolder, item), (err) => {
+        if(err) console.log('error');
+      })
+    });
+  })
+
+  fs.readdir(pathToSourceFolder, (err, files) => {
+    files.forEach(item => {
+      fs.copyFile(path.join(pathToSourceFolder, item), path.join(pathToGoalFolder, item), err => {
+        if(err) {
+          console.log(err);
+        }
+      });
+    })
+  });
+} catch (error) {
+  console.log(error.message);
+}
